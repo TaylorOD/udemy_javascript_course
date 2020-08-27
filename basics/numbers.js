dbe2c1d34b64cab582d75ab5ce17042500d8eb73
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-08-27 09:52:55 -0500

    add numbers file

---
+++ basics/numbers.js
@@ -0,0 +1,13 @@
+let age = 28
+
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+
+let studentScore = 95
+let maxScore = 100
+
+let percent = (studentScore / maxScore) * 100
+
+console.log(percent)
