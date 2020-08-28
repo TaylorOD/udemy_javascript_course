ac2589799d311f9105af6afd1333e514673426b5
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-08-28 11:59:43 -0500

    add temp_warning if statements

---
+++ basics/temp_warning.js
@@ -0,0 +1,10 @@
+
+let temp = 45
+
+if (temp <= 31) {
+  console.log("It's freezing outside!")
+} else if (temp >= 120) {
+  console.log("It's too hot outside!")
+} else {
+  console.log("Go outside! It's great!")
+}
