7f2bf28098ede776f331a7e45f68270c1841ac24
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-01 16:00:54 -0500

    JavaScript 1st Practice

--- functions/functions_101.js
+++ functions/functions_101.js
@@ -16,14 +16,6 @@ let otherValue = square(10)
 console.log(value)
 console.log(otherValue)
 
-// chllenge area
-
-// convert FahrenheitToCelsius
-
-// call a couple of times (32 -> 0) (68 -> 20)
-
-// print the converted values
-
 let fahrenheitToCelsius = function (num) {
   let result = (num - 32) / 1.8
   return result
