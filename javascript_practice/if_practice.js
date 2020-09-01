b3880edcf7e09ab541a7ab7624da37bc8ab76a87
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-01 16:12:56 -0500

    5th and 6th JavaScript practice add

--- javascript_practice/if_practice.js
+++ javascript_practice/if_practice.js
@@ -1,31 +1,69 @@
-// // # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+// // // // // // # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-// // let variable = 10
+// // // // // // let variable = 10
 
-// // if (variable === 10) {
-// //   console.log(0)
+// // // // // // if (variable === 10) {
+// // // // // //   console.log(0)
+// // // // // // } else {
+// // // // // //   console.log(-1)
+// // // // // // }
+
+// // // // // // # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+// // // // // let variable = 11
+// // // // // if (variable < 10) {
+// // // // //   console.log(-1)
+// // // // // } else if (variable > 10) {
+// // // // //   console.log(1)
+// // // // // } else if (variable === 10) {
+// // // // //   console.log(0)
+// // // // // }
+
+// // // // // # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+// // // // let variableOne = 9
+// // // // let variableTwo = 9
+
+// // // // if (variableOne < 10 && variableTwo < 10) {
+// // // //   console.log(1)
+// // // // } else {
+// // // //   console.log(0)
+// // // // }
+
+// // // // # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+// // // let variable = 15000
+
+// // // if (variable > 9000) {
+// // //   console.log(1)
+// // // } else {
+// // //   console.log(-1)
+// // // }
+
+// // // # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+// // let number = 25
+
+// // if (number < 10) {
+// //   console.log(9)
+// // } else if (number < 20) {
+// //   console.log(19)
+// // } else if (number < 30) {
+// //   console.log(29)
 // // } else {
 // //   console.log(-1)
 // // }
 
-// // # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+// // # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
-// let variable = 11
-// if (variable < 10) {
-//   console.log(-1)
-// } else if (variable > 10) {
-//   console.log(1)
-// } else if (variable === 10) {
-//   console.log(0)
+// let numberOne = 11
+// let numberTwo = 5
+
+// if (numberOne > 10 || numberTwo > 10) {
+//   console.log(100)
+// } else {
+//   console.log(-100)
 // }
 
-// # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+// # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
-let variableOne = 9
-let variableTwo = 9
-
-if (variableOne < 10 && variableTwo < 10) {
-  console.log(1)
-} else {
-  console.log(0)
-}
