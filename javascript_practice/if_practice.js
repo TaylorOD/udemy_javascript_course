516dc97c41ae0c0772d71a31f816976012cf13e2
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-01 16:06:05 -0500

    add 3rd javascript practice

--- javascript_practice/if_practice.js
+++ javascript_practice/if_practice.js
@@ -1,11 +1,31 @@
-// # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+// // # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-// let variable = 10
+// // let variable = 10
 
-// if (variable === 10) {
-//   console.log(0)
-// } else {
+// // if (variable === 10) {
+// //   console.log(0)
+// // } else {
+// //   console.log(-1)
+// // }
+
+// // # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+// let variable = 11
+// if (variable < 10) {
 //   console.log(-1)
+// } else if (variable > 10) {
+//   console.log(1)
+// } else if (variable === 10) {
+//   console.log(0)
 // }
 
-// # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+// # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+let variableOne = 9
+let variableTwo = 9
+
+if (variableOne < 10 && variableTwo < 10) {
+  console.log(1)
+} else {
+  console.log(0)
+}
