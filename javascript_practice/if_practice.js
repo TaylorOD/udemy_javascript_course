3a7c4b3f46ac799365607373ba2e698601844802
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-01 16:14:24 -0500

    add 7th JavaScript problem

--- javascript_practice/if_practice.js
+++ javascript_practice/if_practice.js
@@ -67,3 +67,10 @@
 
 // # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
+let number = -100
+
+if (number < 0) {
+  console.log(1776)
+} else {
+  console.log(1979)
+}
