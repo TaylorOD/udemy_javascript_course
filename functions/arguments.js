18ac620a3810243cba83ac81653f639ed376f9c6
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-02 13:02:23 -0500

    add tip function

--- functions/arguments.js
+++ functions/arguments.js
@@ -12,9 +12,20 @@
 
 // default arguments
 
-let getScoreText = function (name = "Anonymous", score = 0) {
-  return "Name: " + name + " - Score: " + score
+// let getScoreText = function (name = "Anonymous", score = 0) {
+//   return "Name: " + name + " - Score: " + score
+// }
+
+// let scoreText = getScoreText(undefined, 99)
+// console.log(scoreText)
+
+// Challegne area
+// total for bill, tipPercent .5 .1 or .2 tip (5%, 10%, or 20%)
+
+
+let bill = function (total = 0, tip = total * .1) {
+  return "The total cost is " + total + " and you should tip " + tip + "."
 }
 
-let scoreText = getScoreText("Taylor")
-console.log(scoreText)
+let tipAmmounts = bill(595,)
+console.log(tipAmmounts)
