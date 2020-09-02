5e5edc0c3de337788c729596c1ca105a16750a27
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-02 12:25:48 -0500

    add default arguments

--- functions/arguments.js
+++ functions/arguments.js
@@ -1,3 +1,5 @@
+// multiple arguments
+
 let add = function (a, b, c) {
   return a + b + c
 
@@ -6,3 +8,14 @@ let add = function (a, b, c) {
 
 let result = add(10, 20, 100)
 console.log(result)
+
+
+// default arguments
+
+let getScoreText = function (name = "Anonymous", score = 0) {
+  console.log(name)
+  console.log(score)
+
+}
+
+getScoreText()
