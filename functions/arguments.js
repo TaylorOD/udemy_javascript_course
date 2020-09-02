1218b4da6a550aed45b3fa9bc3175b31fa4f539c
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-02 12:28:59 -0500

    add text string

--- functions/arguments.js
+++ functions/arguments.js
@@ -1,21 +1,20 @@
 // multiple arguments
 
-let add = function (a, b, c) {
-  return a + b + c
+// let add = function (a, b, c) {
+//   return a + b + c
 
 
-}
+// }
 
-let result = add(10, 20, 100)
-console.log(result)
+// let result = add(10, 20, 100)
+// console.log(result)
 
 
 // default arguments
 
 let getScoreText = function (name = "Anonymous", score = 0) {
-  console.log(name)
-  console.log(score)
-
+  return "Name: " + name + " - Score: " + score
 }
 
-getScoreText()
+let scoreText = getScoreText("Taylor")
+console.log(scoreText)
