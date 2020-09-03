bd413af9af9d2e9f6e3970be8bfb19331b87e44c
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-03 12:56:54 -0500

    2nd add corrected

--- javascript_practice/strings_practice.js
+++ javascript_practice/strings_practice.js
@@ -5,9 +5,9 @@
 
 // console.log("My name is " + firstName + " " + lastName + ".")
 
-//  2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+//  2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
 let firstName = "Taylor"
 let lastName = "Dorsett"
 
-console.log("My name is #{firstName} #{lastName}.")
+console.log("My name is " + firstName + " " + lastName + ".")
