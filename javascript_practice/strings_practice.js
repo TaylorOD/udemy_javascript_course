c6d9190d6ac7963d56f1ea1f24448a37f473e2f7
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-03 13:02:21 -0500

    why is javascript not like ruby?

--- javascript_practice/strings_practice.js
+++ javascript_practice/strings_practice.js
@@ -7,7 +7,17 @@
 
 //  2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
-let firstName = "Taylor"
-let lastName = "Dorsett"
+// let firstName = "Taylor"
+// let lastName = "Dorsett"
 
-console.log("My name is " + firstName + " " + lastName + ".")
+// console.log("My name is " + firstName + " " + lastName + ".")
+
+// 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+// let userInput = gets.chomp
+
+// if (userInput < "marco") {
+//   console.log("polo")
+// }
+
+// JavaScript doesnt make it easy to do gets.chomp or interpolation like ruby does
