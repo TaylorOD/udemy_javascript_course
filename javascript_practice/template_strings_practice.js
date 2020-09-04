49b29afd851bf34965c6b8864aacef1de93a43e7
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-04 12:25:34 -0500

    4th practice add

--- javascript_practice/template_strings_practice.js
+++ javascript_practice/template_strings_practice.js
@@ -5,10 +5,24 @@
 
 // console.log("Look it's " + firstName + " " + lastName + "!")
 
-//  # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+//  # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using template strings (the ${} operator).
 
 // let firstName = "John"
 // let lastName = "Lackson"
 
 // console.log(`Look it's ${firstName} ${lastName}!`)
 
+//  3. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with template strings (the ${} operator).
+
+// let bookTitle = "The Big Bang"
+// let bookAuthor = "Scientists"
+
+// console.log(`If you believe in the Christian idea of Creation you should also research ${bookTitle} theory popularized by ${bookAuthor}.`)
+
+//  4. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+let cityOne = "Chicago"
+let cityTwo = "Atlanta"
+let cityThree = "Denver"
+
+console.log("I'd rather live in " + cityOne + " or " + cityThree + " than " + cityTwo + ".")
