7601cc5aaa86bd8031653e36872400d408585547
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-04 11:42:25 -0500

    add first object

--- objects/objects_101.js
+++ objects/objects_101.js
@@ -1,7 +1,29 @@
-let myBook = {
-  title: "1984",
-  author: "George Orwell",
-  pageCount: 326
+// let myBook = {
+//   title: "1984",
+//   author: "George Orwell",
+//   pageCount: 326
+// }
+
+// console.log(`${myBook.title} by ${myBook.author}`)
+
+// myBook.title = "Animal Farm"
+
+// console.log(`${myBook.title} by ${myBook.author}`)
+
+// console.log(myBook)
+
+
+// name, age, location
+
+// and print them to screen in sentence 
+
+// increase age by 1 then print message again
+
+let myInfo = {
+  name: "Taylor",
+  age: 28,
+  city: "Chicago"
+
 }
 
-console.log(myBook.title)
+console.log(`My name is ${myInfo.name}, I am ${myInfo.age} and I live in ${myInfo.city}.`)
