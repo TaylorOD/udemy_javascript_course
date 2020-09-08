17e1dbd116ed301739df78e3f616fade9cc2f895
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-08 16:11:11 -0500

    add 10 practice problems

--- javascript_practice/functions_practice.js
+++ javascript_practice/functions_practice.js
@@ -10,25 +10,77 @@
 
 //   2. Write a function that takes in a string and returns the string with all capital letters.Then run the function and print the result.
 
-let allCaps = function (string) {
-  return string.toUpperCase()
-}
-let canYouHearMe = allCaps("can you hear me?")
-console.log(canYouHearMe)
+// let allCaps = function (string) {
+//   return string.toUpperCase()
+// }
+// let canYouHearMe = allCaps("can you hear me?")
+// console.log(canYouHearMe)
 
 //   3. Write a function that takes in two numbers and returns the first number subtracted by the second.Then run the function and print the result.
 
-//   4. Write a function that takes in a number and returns the number times itself.Then run the function and print the result.
+// let subtractTwoNumbers = function (number1, number2) {
+//   return number1 - number2
+// }
 
-//   5. Write a function that takes in a string and returns the first letter of the string.Then run the function and print the result.
+// let subtractionProblem = subtractTwoNumbers(10., 8.)
+// console.log(subtractionProblem)
+
+//   4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+// let timesNumber = function (number1) {
+//   return number1 * number1
+// }
+
+// let tenTimesTen = timesNumber(10)
+// console.log(tenTimesTen)
+
+//   5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+// let firstOfString = function (string) {
+//   return string[0]
+// }
+
+// let gandalf = firstOfString("gandalf")
+// console.log(gandalf)
 
 //   6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between.Then run the function and print the result.
 
-//   7. Write a function that takes in a number and returns the number as a string.Then run the function and print the result.
+// let stringsSpacer = function (string1, string2, string3) {
+//   return string1 + " " + string2 + " " + string3
+// }
+
+// let sandwich = stringsSpacer("Bacon", "Lettuce", "Tomato")
+// console.log(sandwich)
+
+//   7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+// let numberToString = function (number) {
+//   return number.toString()
+// }
+
+// let largeNumber = numberToString(1929)
+// console.log(${largeNumber})
 
 //   8. Write a function that takes in a string and returns the string repeated 5 times.Then run the function and print the result.
 
+// let stringTimesFive = function (string) {
+//   return string + string + string + string + string
+// }
+// let stopYell = stringTimesFive("STOP")
+// console.log(stopYell)
+
 //   9. Write a function that takes in 3 numbers and returns the average(the sum divided by 3.0).Then run the function and print the result.
+
+// let average = function (n1, n2, n3) {
+//   return (n1 + n2 + n3) / 3
+// }
+// let testScores = average(68, 100, 83)
+// console.log(testScores)
 
 //   10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
 
+// let timesTenPlusThirty = function (number) {
+//   return (number * 10) + 30
+// }
+// let fifty = timesTenPlusThirty(50)
+// console.log(fifty)
