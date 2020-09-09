a993d6b303d6a8d8e3f17faec68b6d12a9a54d6c
Taylor Dorsett <taylorsunwatedemails@gmail.com>
2020-09-09 13:32:14 -0500

    challenge exercise

--- objects/objects_references.js
+++ objects/objects_references.js
@@ -7,10 +7,34 @@ let myAcconut = {
 let addExpense = function (account, amount) {
 
   account.expenses = account.expenses + amount
-  console.log(account)
 
 }
 
-addExpense(myAcconut, 2.50)
+let addIncome = function (account, amount) {
+  account.income = account.income + amount
 
+}
+
+let resetAccount = function (account) {
+  account.expenses = 0
+  account.income = 0
+}
+
+let getAccountSummary = function (account) {
+  return `Account for ${account.name} has $${account.income - account.expenses}. $${account.income} in income and $${account.expenses} in expenses.`
+}
+
+addExpense(myAcconut, 2.50)
+addIncome(myAcconut, 500)
+addExpense(myAcconut, 50)
+resetAccount(myAcconut)
+addIncome(myAcconut, 5000)
+addExpense(myAcconut, 1500)
+
+let taylorAccount = getAccountSummary(myAcconut)
+console.log(myAcconut)
+console.log(taylorAccount)
+
+resetAccount(myAcconut)
+console.log(taylorAccount)
 console.log(myAcconut)
