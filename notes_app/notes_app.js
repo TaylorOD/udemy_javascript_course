56f61c16bffcfe9db283260509afd17dcebc65b7
taylorod <taylorsunwatedemails@gmail.com>
2020-09-25 12:22:11 -0500

    update todo app

--- notes_app/notes_app.js
+++ notes_app/notes_app.js
@@ -1,3 +1,18 @@
+
+const notes = [{
+  title: "My next Trip",
+  body: "I would like to go to Spain"
+
+}, {
+  title: "Coding projects",
+  body: "JavaScript and Ruby"
+
+}, {
+  title: "Buy new watch",
+  body: "Gold"
+
+}]
+
 // DOM
 
 // const p = document.querySelector("p")
@@ -10,3 +25,10 @@ ps.forEach(function (p) {
   // console.log(p.textContent)
   // p.remove()
 })
+
+// Add a new element (h1, p), then update content (give it a text value), put it somewhere in the app
+
+const newParagraph = document.createElement("p")
+newParagraph.textContent = "This is a new element from JavaScript"
+document.querySelector("body").appendChild(newParagraph)
+
--- todo_app/todo_app.js
+++ todo_app/todo_app.js
@@ -1,6 +1,49 @@
-const p = document.querySelectorAll("p")
+const todos = [{
+  text: "Practice JavaScript",
+  completed: true
 
-p.forEach(function (parapraph) {
-  if (parapraph.textContent.includes("the"))
-    parapraph.remove()
+}, {
+  text: "Update Resume",
+  completed: true
+
+}, {
+  text: "Finish Blog",
+  completed: false
+
+}, {
+  text: "Wrtie Write Up",
+  completed: false
+
+}, {
+  text: "Send Invoice",
+  completed: true
+
+}]
+
+// You have 2 todos left (p element)
+// Add a p for each todo above (use text value)
+
+
+// const p = document.querySelectorAll("p")
+
+// p.forEach(function (parapraph) {
+//   if (parapraph.textContent.includes("the"))
+//     parapraph.remove()
+// })
+
+// Add a new element (h1, p), then update content (give it a text value), put it somewhere in the app
+
+todos.filter(function (todo) {
+  const p = document.createElement("p")
+  p.textContent = todo.text
+  document.querySelector("body").appendChild(p)
 })
+
+
+const incompleteTodo = todos.filter(function (todo) {
+  return !todo.completed
+})
+
+const summary = document.createElement("h4")
+summary.textContent = `You have ${incompleteTodo.length} todos left`
+document.querySelector("body").appendChild(summary)
