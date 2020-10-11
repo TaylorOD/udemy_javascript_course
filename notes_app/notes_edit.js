5e1e944ded4164822efccd276ae42db7a2acbd5a
taylorod <taylorsunwantedemails@gmail.com>
2020-10-11 11:49:26 -0500

    add window update live changes feature

--- notes_app/notes_edit.js
+++ notes_app/notes_edit.js
@@ -4,8 +4,8 @@ const titleEl = document.querySelector("#note_title")
 const bodyEl = document.querySelector("#note_body")
 const removeEl = document.querySelector("#remove_note")
 const noteID = location.hash.substring(1)
-const notes = getSavedNotes()
-const note = notes.find(function (note) {
+let notes = getSavedNotes()
+let note = notes.find(function (note) {
   return note.id === noteID
 })
 
@@ -28,12 +28,26 @@ bodyEl.addEventListener("input", function (e) {
   saveNotes(notes)
 })
 
-
-
-
 // remove note on edit page and redirect to home page
 removeEl.addEventListener("click", function () {
   removeNote(note.id)
   saveNotes(notes)
   location.assign("index.html")
+})
+
+// syncs across edit pages
+window.addEventListener("storage", function (e) {
+  if (e.key === "notes") {
+    notes = JSON.parse(e.newValue)
+    note = notes.find(function (note) {
+      return note.id === noteID
+    })
+
+    if (note === undefined) {
+      location.assign("index.html")
+    }
+
+    titleEl.value = note.title
+    bodyEl.value = note.body
+  }
 })
