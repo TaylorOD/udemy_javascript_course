817683008b8c3b1ea1b924acd054f53dec3442b1
taylorod <taylorsunwantedemails@gmail.com>
2020-10-12 10:10:51 -0500

    add some dates work

--- notes_app/notes_app.js
+++ notes_app/notes_app.js
@@ -33,3 +33,38 @@ window.addEventListener("storage", function (e) {
     renderNotes(notes, filters)
   }
 })
+
+// const now = new Date("January 21 2001 6:25:01")
+// const timestamp = now.getTime()
+
+// const myDate = new Date(timestamp)
+
+// console.log(myDate.getFullYear())
+
+// console.log(now.toString())
+// console.log(now.getTime())
+
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Day of month: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minutes: ${now.getMinutes()}`)
+// console.log(`Seconds: ${now.getSeconds()}`)
+
+const birthday = new Date("May 20 1992 5:19:30")
+const graduation = new Date("August 15 2010 3:14:20")
+
+console.log(birthday)
+console.log(graduation)
+
+const birthdayTimestamp = birthday.getTime()
+const graduationTimestamp = graduation.getTime()
+
+console.log(birthdayTimestamp)
+console.log(graduationTimestamp)
+
+if (birthdayTimestamp > graduationTimestamp) {
+  console.log(graduation.toString())
+} else if (graduationTimestamp > birthdayTimestamp) {
+  console.log(birthday.toString())
+}
