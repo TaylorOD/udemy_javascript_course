419d5bb22b30c98f9ba900c0189100591cec1678
taylorod <taylorsunwantedemails@gmail.com>
2020-10-17 12:09:52 -0500

    add type coheison practice

---
+++ JavaScript/advanced/type_coercion.js
@@ -0,0 +1,18 @@
+// Type Coercion - a string, a number, a boolean
+
+console.log("5" + 5)
+console.log("5" - 5)
+
+// check for eqaulity but doesnt take type into account
+console.log(5 == "5")
+
+typeof 123
+console.log(typeof 123)
+
+// const type = typeof 123
+// console.log(type)
+
+
+const value = false * 12
+const type = typeof value
+console.log(value)
