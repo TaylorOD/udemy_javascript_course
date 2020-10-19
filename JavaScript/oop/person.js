f0c9e48f57500effff6436319e816d4bafd83698
taylorod <taylorsunwantedemails@gmail.com>
2020-10-19 10:49:00 -0500

    add inheritance practice

--- JavaScript/oop/person.js
+++ JavaScript/oop/person.js
@@ -1,13 +1,26 @@
+// Prototypal Inheritance - 
+
 const Person = function (firstName, lastName, age) {
   this.firstName = firstName  
   this.lastName = lastName
   this.age = age
 }
 
+Person.prototype.getBio = function () {
+  return `${this.firstName} is ${this.age}.`
+}
+
+Person.prototype.setName = function (fullName) {
+  const names = fullName.split(" ")
+  this.firstName = names[0]
+  this.lastName = names[1]
+}
+
 
 const me = new Person("Taylor", "Dorsett", 28)
-console.log(me)
+me.setName("Tanner Johnson")
+console.log(me.getBio())
 
 const person2 = new Person("Todd", "Dobbie", 25)
-console.log(person2)
+console.log(person2.getBio())
 
