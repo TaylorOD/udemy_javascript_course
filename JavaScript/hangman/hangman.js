c0bae00b83f6d7c3a60c2c4a3ea9534df00b5b1c
taylorod <taylorsunwantedemails@gmail.com>
2020-10-20 13:40:12 -0500

    add guessing function

---
+++ JavaScript/hangman/hangman.js
@@ -0,0 +1,51 @@
+const Hangman = function (word, allowedGuesses) {
+  this.word = word.toLowerCase().split("")
+  this.allowedGuesses = allowedGuesses
+  this.guessedLetters = []
+}
+
+Hangman.prototype.getPuzzle = function () {
+  let puzzle = ""
+  this.word.forEach((letter) => {
+    if (this.guessedLetters.includes(letter) || letter === " ") {
+      puzzle += letter
+
+    } else {
+      puzzle += "*"
+
+    }
+  })
+  return puzzle
+}
+
+Hangman.prototype.makeGuess = function (guess) {
+  guess = guess.toLowerCase()
+  const isUnique = !this.guessedLetters.includes(guess)
+  const isBadGuess = !this.word.includes(guess)
+
+  if (isUnique) {
+    this.guessedLetters.push(guess)
+
+  }  
+  if (isUnique && isBadGuess) {
+    this.allowedGuesses -= 1
+  }
+}
+
+
+
+const hangmanOne = new Hangman("cat", 3)
+// guess c, t, z
+hangmanOne.makeGuess("a")
+hangmanOne.makeGuess("t")
+hangmanOne.makeGuess("z")
+hangmanOne.makeGuess("s")
+console.log(hangmanOne.getPuzzle()) //c*t
+console.log(hangmanOne.allowedGuesses)
+// print remaining guesses (should be 1)
+const hangmanTwo = new Hangman("New Jersey", 8)
+hangmanTwo.makeGuess("w")
+console.log(hangmanTwo.getPuzzle())
+console.log(hangmanTwo.allowedGuesses)
+// guess w
+// console.log(hangmanTwo.getPuzzle()) **w *****
