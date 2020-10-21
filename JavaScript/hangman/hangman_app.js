aa0c25c33711b4e25e7a1792518d4f43b75df734
taylorod <taylorsunwantedemails@gmail.com>
2020-10-21 11:06:25 -0500

    refactored code and removed unused

--- JavaScript/hangman/hangman_app.js
+++ JavaScript/hangman/hangman_app.js
@@ -1,31 +1,3 @@
-// Primitive value: string, number , boolean, null, undefined - everything not in this list is an object
-
-// myObject -> Object.prototype -> null
-// Array: myArray -> Array.prototype ->  Object.prototype -> null
-// Function: myFunction -> Function.prototype -> Object.prototype -> null
-// both arrays and functions are modified versions of objects
-// string is not an object but it can use methods because it has an object wrapper. It gets converted to an object by javascript.
-// This makes almost everything in javaScript an object but in reality string, number, and boolean are not but they act like them.
-// String: myString -> String.prototype -> Object.prototype -> null
-// Number: myNumber -> Number.prototype  -> Object.prototype -> null
-// Boolean: myBoolean -> Boolean.prototype -> Object.prototype -> null
-
-// 
-
-// const hangmanOne = new Hangman("cat", 3)
-// guess c, t, z
-// hangmanOne.makeGuess("a")
-// hangmanOne.makeGuess("t")
-// hangmanOne.makeGuess("z")
-// hangmanOne.makeGuess("s")
-
-
-// const hangmanTwo = new Hangman("New Jersey", 8)
-// hangmanTwo.makeGuess("w")
-// console.log(hangmanTwo.getPuzzle())
-// console.log(hangmanTwo.allowedGuesses)
-
-
 const puzzleEl = document.querySelector("#puzzle")
 const guessesEl = document.querySelector("#guesses")
 
@@ -40,6 +12,3 @@ window.addEventListener("keypress", function (e) {
   puzzleEl.textContent = hangmanOne.getPuzzle()
   guessesEl.textContent = hangmanOne.allowedGuesses
 })
-
-// renderHangmanGames(hangmanOne.getPuzzle())
-// renderHangmanGuesses(hangmanOne.allowedGuesses)
