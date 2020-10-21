9c743100e71e7fcbc197ce56d2f3aad6aace6214
taylorod <taylorsunwantedemails@gmail.com>
2020-10-21 11:46:46 -0500

    add code so it displays in broswer

--- JavaScript/hangman/hangman_app.js
+++ JavaScript/hangman/hangman_app.js
@@ -6,15 +6,15 @@ const hangmanOne = new Hangman("cat", 3)
 
 puzzleEl.textContent = hangmanOne.getPuzzle()
 guessesEl.textContent = hangmanOne.allowedGuesses
-// statusEl.textContent = hangmanOne.status
-console.log(hangmanOne.getStatus())
+statusEl.textContent = hangmanOne.getStatus()
+// console.log(hangmanOne.getStatus())
 
 window.addEventListener("keypress", function (e) {
   const guess = String.fromCharCode(e.charCode)
   hangmanOne.makeGuess(guess)
   puzzleEl.textContent = hangmanOne.getPuzzle()
   guessesEl.textContent = hangmanOne.allowedGuesses
-  // statusEl.textContent = hangmanOne.status
-  console.log(hangmanOne.getStatus())
+  statusEl.textContent = hangmanOne.getStatus()
+  // console.log(hangmanOne.getStatus())
 
 })
