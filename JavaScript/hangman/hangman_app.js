fd8f693c8ed9c64adc3d5f453e217008abdd9e13
taylorod <taylorsunwantedemails@gmail.com>
2020-10-22 10:56:04 -0500

    refactor code with class syntax

--- JavaScript/hangman/hangman_app.js
+++ JavaScript/hangman/hangman_app.js
@@ -5,7 +5,6 @@ const statusEl = document.querySelector("#status");
 const hangmanOne = new Hangman("cat", 3);
 
 puzzleEl.textContent = hangmanOne.getPuzzle();
-guessesEl.textContent = hangmanOne.allowedGuesses;
 statusEl.textContent = hangmanOne.getStatus();
 
 
@@ -14,7 +13,6 @@ window.addEventListener("keypress", function (e) {
   hangmanOne.makeGuess(guess);
   hangmanOne.calculateStatus()
   puzzleEl.textContent = hangmanOne.getPuzzle();
-  guessesEl.textContent = hangmanOne.allowedGuesses;
   statusEl.textContent = hangmanOne.getStatus();
 
 });
