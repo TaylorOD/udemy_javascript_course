796601cdb4415bb310fc6a03e5df4a682bf316fa
taylorod <taylorsunwantedemails@gmail.com>
2020-10-23 10:33:47 -0500

    add subclass to person class

--- JavaScript/oop/person.js
+++ JavaScript/oop/person.js
@@ -23,32 +23,29 @@ class Person {
   }
 }
 
+class Employee extends Person {
+  constructor(firstName, lastName, age, position, likes) {
+    super(firstName, lastName, age, likes)
+    this.position = position
+  }
+  getBio() {
+    return `${this.firstName} ${this.lastName} is a ${this.position}.`
+  }
+  getYearsLeft() {
+    return 65 - this.age
+  }
+}
+
+
+
+
+
 const myPerson = new Person("Taylor", "Dorsett", 28, ["Codin"])
 console.log(myPerson.getBio())
 
-
-// const Person = function (firstName, lastName, age, likes = []) {
-//   this.firstName = firstName  
-//   this.lastName = lastName
-//   this.age = age
-//   this.likes = likes
-// }
-
-// Person.prototype.getBio = function () {
-//   let bio = `${this.firstName} is ${this.age}.`
-
-//   this.likes.forEach((like) => {
-//     bio += ` ${this.firstName} likes ${like}.`
-//   })
-
-//   return bio
-// }
-
-// Person.prototype.setName = function (fullName) {
-//   const names = fullName.split(" ")
-//   this.firstName = names[0]
-//   this.lastName = names[1]
-// }
+const myEmployee = new Employee("Taylor", "Dorsett", 28, "Software Developer", ["Codin"])
+console.log(myEmployee.getBio())
+console.log(myEmployee.getYearsLeft())
 
 const me = new Person("Taylor", "Dorsett", 28, ["Climbing", "Walking"])
 me.setName("Tanner Johnson")
