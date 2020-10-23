cbca1f4d2f40987b6ba08a7a46921e4d0bb390a3
taylorod <taylorsunwantedemails@gmail.com>
2020-10-23 10:43:56 -0500

    add student subclass and new methods

--- JavaScript/oop/person.js
+++ JavaScript/oop/person.js
@@ -36,21 +36,45 @@ class Employee extends Person {
   }
 }
 
+class Student extends Person {
+  constructor(firstName, lastName, age, likes, grade, status) {
+    super(firstName, lastName, age, likes)
+    this.grade = grade
+    this.status = status
+  }
+  getStatus() {
+    if (this.grade > 70) {
+      this.status = "passing"
+    } else if (this.grade < 70) {
+      this.status = "failing"
+    }
+    return this.status
+  }
+  getBio() {
+    return `${this.firstName} is ${this.getStatus()} this class.`
+  }
+  updateGrade(grade) {
+    this.grade += grade
+  }
+}
 
+const student1 = new Student("Tray", "Narcovik", 34, ["studying", "learning"], 95)
+console.log(student1)
+console.log(student1.getBio())
+student1.updateGrade(-40)
+console.log(student1.getBio())
 
+// const myPerson = new Person("Taylor", "Dorsett", 28, ["Codin"])
+// console.log(myPerson.getBio())
 
+// const myEmployee = new Employee("Taylor", "Dorsett", 28, "Software Developer", ["Codin"])
+// console.log(myEmployee.getBio())
+// console.log(myEmployee.getYearsLeft())
 
-const myPerson = new Person("Taylor", "Dorsett", 28, ["Codin"])
-console.log(myPerson.getBio())
+// const me = new Person("Taylor", "Dorsett", 28, ["Climbing", "Walking"])
+// me.setName("Tanner Johnson")
+// console.log(me.getBio())
 
-const myEmployee = new Employee("Taylor", "Dorsett", 28, "Software Developer", ["Codin"])
-console.log(myEmployee.getBio())
-console.log(myEmployee.getYearsLeft())
-
-const me = new Person("Taylor", "Dorsett", 28, ["Climbing", "Walking"])
-me.setName("Tanner Johnson")
-console.log(me.getBio())
-
-const person2 = new Person("Todd", "Dobbie", 25)
-console.log(person2.getBio())
+// const person2 = new Person("Todd", "Dobbie", 25)
+// console.log(person2.getBio())
 
