53a4f103b93ce65605d8509a066064b645cf5e5a
taylorod <taylorsunwantedemails@gmail.com>
2020-10-24 11:53:46 -0500

    add sad path for http request

--- JavaScript/hangman/hangman_app.js
+++ JavaScript/hangman/hangman_app.js
@@ -9,7 +9,7 @@ const hangmanOne = new Hangman("cat dog", 3);
 puzzleEl.textContent = hangmanOne.puzzle;
 statusEl.textContent = hangmanOne.statusMessage;
 
-window.addEventListener("keypress", function (e) {
+window.addEventListener("keypress", (e) => {
   const guess = String.fromCharCode(e.charCode);
 
   hangmanOne.makeGuess(guess);
@@ -22,9 +22,11 @@ window.addEventListener("keypress", function (e) {
 const request = new XMLHttpRequest()
 
 request.addEventListener("readystatechange", (e) => {
-  if (e.target.readyState === 4) {
+  if (e.target.readyState === 4 && e.target.status === 200) {
     const data = JSON.parse(e.target.responseText)
     console.log(data)
+  } else if (e.target.readyState === 4) {
+    console.log("Error has taken place")
   }
 })
 
