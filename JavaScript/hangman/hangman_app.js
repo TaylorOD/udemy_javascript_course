8c322ca124c5399f5aa082e45ea3903fce3b12df
taylorod <taylorsunwantedemails@gmail.com>
2020-10-26 12:11:22 -0500

    make countryDate a callback async function as well

--- JavaScript/hangman/hangman_app.js
+++ JavaScript/hangman/hangman_app.js
@@ -19,7 +19,7 @@ window.addEventListener("keypress", (e) => {
 
 });
 
-getPuzzle((error, puzzle) => {
+getPuzzle("2", (error, puzzle) => {
   if (error) {
     console.log(`Error: ${error}`)
   } else {
@@ -27,22 +27,10 @@ getPuzzle((error, puzzle) => {
   }
 })
 
-// const countryCode = "US"
-// const countryRequest = new XMLHttpRequest()
-
-// countryRequest.addEventListener("readystatechange", (e) => {
-//   if (e.target.readyState === 4 && e.target.status === 200) {
-//     const countryData = JSON.parse(e.target.responseText)
-
-//     countryData.forEach((country) => {
-//       if (country.alpha2Code === countryCode) {
-//         console.log(country.name)
-//       } 
-//     })
-//   } else if (e.target.readyState === 4) {
-//     console.log("An error has taken place.")
-//   }
-// })
-
-// countryRequest.open("GET", "http://restcountries.eu/rest/v2/all")
-// countryRequest.send()
+getCountry("US", (error, countryName) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(countryName)
+  }
+})
--- JavaScript/hangman/requests.js
+++ JavaScript/hangman/requests.js
@@ -1,4 +1,4 @@
-const getPuzzle = (callback) => {
+const getPuzzle = (wordCount, callback) => {
   
   const request = new XMLHttpRequest()
 
@@ -11,25 +11,52 @@ const getPuzzle = (callback) => {
     }
   })
 
-
-
-  request.open("GET", "http://puzzle.mead.io/puzzle")
+  request.open("GET", `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
   request.send()
 
 }
 
 
-const getPuzzleSync = () => {
-  const request = new XMLHttpRequest()
+const getCountry = (countryCode, callback) => {
+  
+  const countryRequest = new XMLHttpRequest()
 
-  request.open("GET", "http://puzzle.mead.io/puzzle", false)
-  request.send()
+  countryRequest.addEventListener("readystatechange", (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+      const countryData = JSON.parse(e.target.responseText)
+      
+      countryData.forEach((country) => {
+        if (country.alpha2Code === countryCode) {
+          callback(undefined, country.name)
+        } 
+      })
+    } else if (e.target.readyState === 4) {
+      callback("An error has taken place.", undefined)
+    }
+  })
 
-  if (request.readyState === 4 && request.status === 200) {
-    const data = JSON.parse(request.responseText)
-    return data.puzzle
-  } else if (request.readyState === 4) {
-    throw new Error("Things did not go well")
-  }
-
+  countryRequest.open("GET", "http://restcountries.eu/rest/v2/all")
+  countryRequest.send()
+ 
 }
+
+
+// const countryCode = "US"
+
+
+
+
+// const getPuzzleSync = () => {
+//   const request = new XMLHttpRequest()
+
+//   request.open("GET", "http://puzzle.mead.io/puzzle", false)
+//   request.send()
+
+//   if (request.readyState === 4 && request.status === 200) {
+//     const data = JSON.parse(request.responseText)
+//     return data.puzzle
+//   } else if (request.readyState === 4) {
+//     throw new Error("Things did not go well")
+//   }
+
+// }
