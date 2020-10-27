50fa5b272884deea2ba834d0632ce3f9a826d994
taylorod <taylorsunwantedemails@gmail.com>
2020-10-27 10:16:46 -0500

    add more closure examples

--- JavaScript/functions/closures.js
+++ JavaScript/functions/closures.js
@@ -1,10 +1,43 @@
-const myFunction = () => {
-  const message = "this is my message"
-  const printMessage = () => {
-    console.log(message)
+// const myFunction = () => {
+//   const message = "this is my message"
+//   const printMessage = () => {
+//     console.log(message)
+//   }
+//   return printMessage
+// }
+
+// const myPrintMessage = myFunction()
+// myPrintMessage()
+
+
+const createCounter = () => {
+  let count = 0
+
+  return {
+    increment() {
+      count += 1
+    },
+    decrement() {
+      count -= 1
+    },
+    get() {
+      return count
+    }
   }
-  return printMessage
 }
 
-const myPrintMessage = myFunction()
-myPrintMessage()
+const counter = createCounter()
+counter.increment()
+counter.decrement()
+counter.decrement()
+console.log(counter.get())
+
+// adder
+const createAdder = (a) => {
+  return (b) => {
+    return a + b
+  }
+}
+const add10 = createAdder(10)
+console.log(add10(-2))
+console.log(add10(20))
