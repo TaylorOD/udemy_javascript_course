8532c867b2013066169bebfc4c35d6e7bf525536
taylorod <taylorsunwantedemails@gmail.com>
2020-10-29 11:18:08 -0500

    add async functions into hangman game

--- hangman/hangman_app.js
+++ hangman/hangman_app.js
@@ -1,35 +1,48 @@
-
-
 const puzzleEl = document.querySelector("#puzzle");
 const guessesEl = document.querySelector("#guesses");
 const statusEl = document.querySelector("#status");
 
-const hangmanOne = new Hangman("cat dog", 3);
+let hangmanOne
 
-puzzleEl.textContent = hangmanOne.puzzle;
-statusEl.textContent = hangmanOne.statusMessage;
+// puzzleEl.textContent = hangmanOne.puzzle;
+// statusEl.textContent = hangmanOne.statusMessage;
 
 window.addEventListener("keypress", (e) => {
-  const guess = String.fromCharCode(e.charCode);
+  const guess = String.fromCharCode(e.charCode)
 
-  hangmanOne.makeGuess(guess);
+  hangmanOne.makeGuess(guess)
   hangmanOne.calculateStatus()
+  render()
+  
+})
+
+const render = () => {
   puzzleEl.textContent = hangmanOne.puzzle;
   statusEl.textContent = hangmanOne.statusMessage;
+}
 
-});
+const startGame = async () => {
+  const puzzle = await getPuzzle("2")
+  hangmanOne = new Hangman(puzzle, 5)
+  render()
+}
 
-getPuzzle("2").then((puzzle) => {
-  console.log(puzzle)
-}).catch((err) => {
-  console.log(`Error: ${err}`)
-})
+document.querySelector("#reset").addEventListener("click", startGame)
 
-getCurrentCountry().then((country) => {
-  console.log(country.name)
-}).catch((error) => {
-  console.log(error)
-})
+startGame()
+
+
+// getPuzzle("2").then((puzzle) => {
+//   console.log(puzzle)
+// }).catch((err) => {
+//   console.log(`Error: ${err}`)
+// })
+
+// getCurrentCountry().then((country) => {
+//   console.log(country.name)
+// }).catch((error) => {
+//   console.log(error)
+// })
 
 // getLocation().then((location) => {
 //   console.log(`You are located at ${location.city}, ${location.region} ${location.country}`)
