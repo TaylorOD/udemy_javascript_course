b485c929db427b65bb2c617a581cd8b6c0d27b6d
taylorod <taylorsunwantedemails@gmail.com>
2020-11-19 10:23:42 -0600

    add rest param example

--- boilerplate copy/src/index.js
+++ boilerplate copy/src/index.js
@@ -1,5 +1,9 @@
-const calculateAverage = (numOne, numTwo) => {
-  return (numOne + numTwo) / 2
+const calculateAverage = (thing, ...numbers) => {
+  // return (numOne + numTwo) / 2
+  let sum = 0
+  numbers.forEach((num) => sum += num)
+  const average = sum / numbers.length
+  return `the average ${thing} is ${average}`
 }
 
-console.log(calculateAverage(0, 100))
+console.log(calculateAverage("grade", 0, 100, 88, 94))
