d7516f92fb72fa9f420b1aaa1c6a8c6462551d3b
taylorod <taylorsunwantedemails@gmail.com>
2020-11-19 10:49:34 -0600

    add spread syntax example

--- boilerplate copy/src/index.js
+++ boilerplate copy/src/index.js
@@ -1,23 +1,23 @@
-const calculateAverage = (thing, ...numbers) => {
-  // return (numOne + numTwo) / 2
-  let sum = 0
-  numbers.forEach((num) => sum += num)
-  const average = sum / numbers.length
-  return `the average ${thing} is ${average}`
-}
+// const calculateAverage = (thing, ...numbers) => {
+//   // return (numOne + numTwo) / 2
+//   let sum = 0
+//   numbers.forEach((num) => sum += num)
+//   const average = sum / numbers.length
+//   return `the average ${thing} is ${average}`
+// }
 
-console.log(calculateAverage("age", 0, 100, 88, 94, 55, 13, 19))
+// console.log(calculateAverage("age", 0, 100, 88, 94, 55, 13, 19))
 
-const printTeam = (team, coach, ...players) => {
-  // console.log(team)
-  // console.log(coach)
-  let playersArray = []
-  players.forEach((player) => playersArray.push((" " + player)))
-  // console.log(playersArray)
-  console.log(`The team is ${team} coached by ${coach} and the players are ${playersArray}.`)
-}
+// const printTeam = (team, coach, ...players) => {
+//   // console.log(team)
+//   // console.log(coach)
+//   let playersArray = []
+//   players.forEach((player) => playersArray.push((" " + player)))
+//   // console.log(playersArray)
+//   console.log(`The team is ${team} coached by ${coach} and the players are ${playersArray}.`)
+// }
 
-printTeam("Liberty", "Casey Penn", "Marge", "Aiden", "Herbert", "Sherry", "Todd", "Luke")
+// printTeam("Liberty", "Casey Penn", "Marge", "Aiden", "Herbert", "Sherry", "Todd", "Luke")
 
 
 const printTeamm = (team, coach, ...players) => {
@@ -30,3 +30,10 @@ const printTeamm = (team, coach, ...players) => {
 }
 
 printTeamm("Liberty", "Casey Penn", "Marge", "Aiden", "Herbert", "Sherry", "Todd", "Luke")
+
+const team = {
+  name :"Liberty",
+  coach: "Casey Penn",
+  players: ["Marge", "Aiden", "Herbert", "Sherry", "Todd", "Luke"]
+}
+printTeamm(team.name, team.coach, ...team.players)
